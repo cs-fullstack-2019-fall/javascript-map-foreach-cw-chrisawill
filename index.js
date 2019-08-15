3b22dfa3ebf02955a90baf9c6ac0e166e3f155e8
Christian Williams <chrisawill05@gmail.com>
2019-08-15 12:59:01 -0500

    Assignment for the morning

---
+++ index.js
@@ -0,0 +1,44 @@
+
+// ### Problem 1
+// Ask the user to enter a word. Keep asking them to enter a word until they enter the letter 'q'.
+
+let user = [""];
+
+while (user !== "q")
+{
+    var wordC= prompt("Enter a word. Press 'q' to quit.");
+    wordC.forEach(function (print)
+    {
+        console.log(print)
+    });
+}
+
+// return console.log("q");
+
+
+//     ### Problem 2
+// Make any string array all lower case strings. If you don't remember how to use that Google it.
+
+
+var string =  [""];
+ string.map(function (element)
+ {
+var toLowerCase = toLowerCase;
+console.log(string)
+
+ });
+
+
+
+
+
+// ### Challenge
+// Make any string array return an array of objects with the specified class below
+//
+// ```
+// class Student{
+//         constructor(name){
+//                 this.name = name;
+//         }
+// }
+// ```
